078dfc52df99f5b1dd99d8408990485966d68cbf
victorjourne <journe.victor@gmail.com>
2021-12-31 14:45:54 +0100

    Css style for form

--- CCLabeler/forms.py
+++ CCLabeler/forms.py
@@ -3,7 +3,7 @@ from . import utils
 from pathlib import Path
 class UploadFileForm(forms.Form):
     #name = forms.CharField(max_length=50, label='name')
-    userdir = Path(utils.userdir)
+    userdir = Path(utils.userdir,  label='File')
     CHOICES = [(file.name, file.stem) for file in userdir.glob('*.json')]
-    user = forms.ChoiceField(choices=CHOICES, label='user', widget=forms.Select)
+    user = forms.ChoiceField(choices=CHOICES, label='User', widget=forms.Select)
     file = forms.FileField()
