d036db8d6f351d226052520a3e4728e6cb6ccbc0
victorjourne <journe.victor@gmail.com>
2022-01-10 10:09:38 +0100

    Add ping.json

--- CCLabeler/urls.py
+++ CCLabeler/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     re_path(r'^summary$', view.summary),
     path('upload/', view.upload, name="upload"),
     path('generate_golden_dataframe/', view.generate_golden_dataframe, name="generate_golden_dataframe")
+    path('disconnect/' , view.disconnect),
+    path('ping' , view.ping),
     #path('image_upload', view.image_view, name = 'image_upload'),
     #path('success', view.success, name = 'success'),
 ]
